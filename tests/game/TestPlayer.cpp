280ace16ed5b480c32b4d4a5b7d1cab1b516e6fa
Zelourses <24527982+Zelourses@users.noreply.github.com>
2024-05-26 02:04:34 +0300

    Setup test environment for future tests
    Added:
     - tests folder with configured example test
    
    Changed:
     - Structure of main `CMakeLists.txt` now a little bit different. it
       puts everything into variables, so underlying tests can link with
       library

---
+++ tests/game/TestPlayer.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+
+#include "player.hpp"
+#include "item.hpp"
+
+int main() {
+    constexpr size_t SWORD_ID = 1;
+    Player p;
+    assert(p.maxHealth == p.calcMaxHealth());
+
+    assert(p.calcAttack() == p.attack);
+    p.hand = SWORD_ID;
+
+    assert(p.calcAttack() == getItemsDict().at(SWORD_ID).wearInfo->attackModifier.value()(p.attack));
+    
+}
